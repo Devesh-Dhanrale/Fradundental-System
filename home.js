ca9523ce7005dca0be6f73bdd940d5df0fc30b51
Devesh-Dhanrale <46558221+Devesh-Dhanrale@users.noreply.github.com>
2019-02-02 01:48:15 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,19 @@
+	
+function validation(){
+	
+	var nm=document.getElementById("en").value;
+	var pn=document.getElementById("pa").value;
+	if(nm.length()<8) 
+	{
+		alert("Please enter the correct password");
+	} 
+	else{
+		
+	}
+}
+
+
+
+
+
+
